ec734a314854395c2f7debc1fd029f88e1ad98d9
Nick Dainty <nick@abelsoninfo.com>
2011-03-23 19:02:44 +0000

    Make thread test run on 1.9.2.

--- misc/thread_test.rb
+++ misc/thread_test.rb
@@ -1,7 +1,7 @@
-require File.join(File.dirname(__FILE__), "../lib/timedcache")
+require File.expand_path('../lib/timedcache', File.dirname(__FILE__))
 
-# This script will almost certainly raise an exception if 
-# thread safety is not implemented.
+# This script will almost certainly raise an exception if the cache is not
+# thread safe.
 
 db = File.join(File.dirname(__FILE__), "thread_test.db")
 
